8ece9e66d9ddeca4dc27cfeaecdeb535c5a641cf
milani97 <felipe.s.milani@hotmail.com>
2023-05-24 18:44:11 -0300

    commit de testes

--- src/app/map-page/map-page.component.ts
+++ src/app/map-page/map-page.component.ts
@@ -106,6 +106,19 @@ export class MapPageComponent implements OnInit {
     });
   }
 
+  myInterval = window.setInterval(function(){
+  navigator.geolocation.getCurrentPosition(position => {
+    this.myLoc = position.coords;
+    console.log(this.myLoc.latitude)
+    console.log(this.myLoc.longitude)
+
+    this.source = {
+      lat: this.myLoc.latitude,
+      lng: this.myLoc.longitude
+    };
+  })
+  }, 5000);
+
   setRoutePolyline() {
     let request = {
       origin: this.source,
